1ffd213a8b5103b3d7f3971a802182c3fd255195
Martin Georgiev <i426060@fhict.nl>
2020-01-07 22:47:22 +0100

    Implemented first iteration of cleaning schedule calendar

---
+++ AdvancedProject1.0/AdvancedProject1.0/CalendarItem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProject1._0
+{
+    public partial class CalendarItem : UserControl
+    {
+        public CalendarItem()
+        {
+            InitializeComponent();
+        }
+
+        private string _date;
+        private string _dayofweek;
+
+        public string Date
+        {
+            get { return _date; }
+            set { _date = value; lblDate.Text = _date; }
+        }
+
+        public string DayOfWeek
+        {
+            get { return _dayofweek; }
+            set { _dayofweek = value; lblWeekDay.Text = _dayofweek; }
+        }
+
+        public void AddEvent(string title, Color eventColor, Color textColor)
+        {
+            calendarEventItem newEvent = new calendarEventItem(calendarEventList);
+            newEvent.Title = title;
+            newEvent.Color = eventColor;
+            newEvent.TextColor = textColor;
+            calendarEventList.Controls.Add(newEvent);
+        }
+
+        public void AddEvent(string title, Color eventColor)
+        {
+            calendarEventItem newEvent = new calendarEventItem(calendarEventList);
+            newEvent.Title = title;
+            newEvent.Color = eventColor;
+            calendarEventList.Controls.Add(newEvent);
+        }
+        
+        public void IsToday()
+        {
+            calendarDateBackground.BackColor = Color.BlueViolet;
+            eventTablePanel.BackColor = SystemColors.ControlLight;
+            WeekDayPanel.BackColor = SystemColors.ControlLight;
+        }
+
+        public void IsToday(Color barColor)
+        {
+            calendarDateBackground.BackColor = barColor;
+            eventTablePanel.BackColor = SystemColors.ControlLight;
+            WeekDayPanel.BackColor = SystemColors.ControlLight;
+        }
+
+        public void IsToday(Color barColor, Color backColor)
+        {
+            calendarDateBackground.BackColor = barColor;
+            eventTablePanel.BackColor = backColor;
+            WeekDayPanel.BackColor = backColor;
+        }
+
+        private void calendarEventList_ControlRemoved(object sender, ControlEventArgs e)
+        {
+            //Resetting scrollbar on control(control) removal
+            //eventTablePanel.VerticalScroll.Value = eventTablePanel.VerticalScroll.Maximum;
+            //eventTablePanel.PerformLayout();
+            //Not working properly = To be fixed()
+        }
+
+    }
+}
---
+++ AdvancedProject1.0/AdvancedProject1.0/calendarEventItem.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProject1._0
+{
+    public partial class calendarEventItem : UserControl
+    {
+        FlowLayoutPanel parentPanel = new FlowLayoutPanel();
+        public calendarEventItem(FlowLayoutPanel passedPanel)
+        {
+            InitializeComponent();
+            parentPanel = passedPanel;
+            this.Dock = DockStyle.Top;
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+        }
+
+        private Color _panelColor;
+        private Color _textColor;
+        private string _title;
+
+        public Color Color
+        {
+            get { return _panelColor; }
+            set { _panelColor = value; this.BackColor = _panelColor; }
+        }
+
+        public Color TextColor
+        {
+            get { return _textColor; }
+            set { _textColor = value; lblTitle.ForeColor = _textColor; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; lblTitle.Text = _title; }
+        }
+
+        private void lblTitle_MouseEnter(object sender, EventArgs e)
+        {
+            //Changing background color on mouse hover
+            this.BackColor = ControlPaint.Light(this.BackColor, 0.25f);
+        }
+
+        private void lblTitle_MouseLeave(object sender, EventArgs e)
+        {
+            //Changing background color back to normal
+            this.BackColor = _panelColor;
+        }
+
+        private void lblTitle_DoubleClick(object sender, EventArgs e)
+        {
+            //Double-clicking item to mark it as complete
+            _panelColor = Color.LawnGreen;
+            _textColor = Color.Black;
+            this.BackColor = _panelColor;
+            lblTitle.ForeColor = _textColor;
+            parentPanel.Controls.Remove(this);
+        }
+    }
+}
