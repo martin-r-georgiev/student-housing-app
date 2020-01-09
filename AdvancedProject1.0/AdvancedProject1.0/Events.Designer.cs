38b9e70bc62c5ee23323e332fbfc39405438ffb5
Martin Georgiev <i426060@fhict.nl>
2020-01-09 11:24:25 +0100

    Removed unneeded radio buttons from Event form

--- AdvancedProject1.0/AdvancedProject1.0/Events.Designer.cs
+++ AdvancedProject1.0/AdvancedProject1.0/Events.Designer.cs
@@ -45,8 +45,6 @@
             this.label6 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.btnAddEvent = new System.Windows.Forms.Button();
-            this.rbAdmin = new System.Windows.Forms.RadioButton();
-            this.rbTenant = new System.Windows.Forms.RadioButton();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
@@ -61,17 +59,19 @@
             this.panel1.Controls.Add(this.lblDate);
             this.panel1.Controls.Add(this.lblHeader);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 55);
+            this.panel1.Location = new System.Drawing.Point(16, 68);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(502, 110);
+            this.panel1.Size = new System.Drawing.Size(669, 135);
             this.panel1.TabIndex = 0;
             // 
             // lblInfo
             // 
             this.lblInfo.AutoSize = true;
-            this.lblInfo.Location = new System.Drawing.Point(134, 72);
+            this.lblInfo.Location = new System.Drawing.Point(179, 89);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblInfo.Name = "lblInfo";
-            this.lblInfo.Size = new System.Drawing.Size(25, 13);
+            this.lblInfo.Size = new System.Drawing.Size(31, 17);
             this.lblInfo.TabIndex = 3;
             this.lblInfo.Text = "Info";
             // 
@@ -79,9 +79,10 @@
             // 
             this.lblDate.AutoSize = true;
             this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.lblDate.Location = new System.Drawing.Point(134, 39);
+            this.lblDate.Location = new System.Drawing.Point(179, 48);
+            this.lblDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(119, 20);
+            this.lblDate.Size = new System.Drawing.Size(152, 25);
             this.lblDate.TabIndex = 2;
             this.lblDate.Text = "Date and Time";
             // 
@@ -89,25 +90,28 @@
             // 
             this.lblHeader.AutoSize = true;
             this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.lblHeader.Location = new System.Drawing.Point(132, 3);
+            this.lblHeader.Location = new System.Drawing.Point(176, 4);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(82, 25);
+            this.lblHeader.Size = new System.Drawing.Size(97, 30);
             this.lblHeader.TabIndex = 1;
             this.lblHeader.Text = "Header";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(107, 104);
+            this.pictureBox1.Size = new System.Drawing.Size(143, 128);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Location = new System.Drawing.Point(16, 15);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.Size = new System.Drawing.Size(100, 28);
             this.btnBack.TabIndex = 1;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -119,17 +123,19 @@
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.pictureBox2);
-            this.panel2.Location = new System.Drawing.Point(12, 171);
+            this.panel2.Location = new System.Drawing.Point(16, 210);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(502, 110);
+            this.panel2.Size = new System.Drawing.Size(669, 135);
             this.panel2.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(134, 72);
+            this.label1.Location = new System.Drawing.Point(179, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.Size = new System.Drawing.Size(31, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Info";
             // 
@@ -137,9 +143,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.label2.Location = new System.Drawing.Point(134, 39);
+            this.label2.Location = new System.Drawing.Point(179, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.Size = new System.Drawing.Size(152, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Date and Time";
             // 
@@ -147,17 +154,19 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.label3.Location = new System.Drawing.Point(132, 3);
+            this.label3.Location = new System.Drawing.Point(176, 4);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 25);
+            this.label3.Size = new System.Drawing.Size(97, 30);
             this.label3.TabIndex = 1;
             this.label3.Text = "Header";
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(107, 104);
+            this.pictureBox2.Size = new System.Drawing.Size(143, 128);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
@@ -167,17 +176,19 @@
             this.panel3.Controls.Add(this.label5);
             this.panel3.Controls.Add(this.label6);
             this.panel3.Controls.Add(this.pictureBox3);
-            this.panel3.Location = new System.Drawing.Point(12, 287);
+            this.panel3.Location = new System.Drawing.Point(16, 353);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(502, 110);
+            this.panel3.Size = new System.Drawing.Size(669, 135);
             this.panel3.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(134, 72);
+            this.label4.Location = new System.Drawing.Point(179, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.Size = new System.Drawing.Size(31, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "Info";
             // 
@@ -185,9 +196,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.label5.Location = new System.Drawing.Point(134, 39);
+            this.label5.Location = new System.Drawing.Point(179, 48);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(119, 20);
+            this.label5.Size = new System.Drawing.Size(152, 25);
             this.label5.TabIndex = 2;
             this.label5.Text = "Date and Time";
             // 
@@ -195,64 +207,44 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.label6.Location = new System.Drawing.Point(132, 3);
+            this.label6.Location = new System.Drawing.Point(176, 4);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 25);
+            this.label6.Size = new System.Drawing.Size(97, 30);
             this.label6.TabIndex = 1;
             this.label6.Text = "Header";
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox3.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(107, 104);
+            this.pictureBox3.Size = new System.Drawing.Size(143, 128);
             this.pictureBox3.TabIndex = 0;
             this.pictureBox3.TabStop = false;
             // 
             // btnAddEvent
             // 
-            this.btnAddEvent.Location = new System.Drawing.Point(537, 335);
+            this.btnAddEvent.Location = new System.Drawing.Point(716, 412);
+            this.btnAddEvent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAddEvent.Name = "btnAddEvent";
-            this.btnAddEvent.Size = new System.Drawing.Size(161, 59);
+            this.btnAddEvent.Size = new System.Drawing.Size(215, 73);
             this.btnAddEvent.TabIndex = 5;
             this.btnAddEvent.Text = "Add Event";
             this.btnAddEvent.UseVisualStyleBackColor = true;
             this.btnAddEvent.Click += new System.EventHandler(this.btnAddEvent_Click);
             // 
-            // rbAdmin
-            // 
-            this.rbAdmin.AutoSize = true;
-            this.rbAdmin.Location = new System.Drawing.Point(120, 15);
-            this.rbAdmin.Name = "rbAdmin";
-            this.rbAdmin.Size = new System.Drawing.Size(54, 17);
-            this.rbAdmin.TabIndex = 6;
-            this.rbAdmin.TabStop = true;
-            this.rbAdmin.Text = "Admin";
-            this.rbAdmin.UseVisualStyleBackColor = true;
-            // 
-            // rbTenant
-            // 
-            this.rbTenant.AutoSize = true;
-            this.rbTenant.Location = new System.Drawing.Point(236, 15);
-            this.rbTenant.Name = "rbTenant";
-            this.rbTenant.Size = new System.Drawing.Size(59, 17);
-            this.rbTenant.TabIndex = 7;
-            this.rbTenant.TabStop = true;
-            this.rbTenant.Text = "Tenant";
-            this.rbTenant.UseVisualStyleBackColor = true;
-            // 
             // Events
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 405);
-            this.Controls.Add(this.rbTenant);
-            this.Controls.Add(this.rbAdmin);
+            this.ClientSize = new System.Drawing.Size(948, 498);
             this.Controls.Add(this.btnAddEvent);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Events";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Events";
@@ -266,7 +258,6 @@
             this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -289,7 +280,5 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Button btnAddEvent;
-        private System.Windows.Forms.RadioButton rbAdmin;
-        private System.Windows.Forms.RadioButton rbTenant;
     }
 }
